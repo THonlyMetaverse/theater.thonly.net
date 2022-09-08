2d65c0053aa6bf06ddf572d932769224c66fa41a
Thon Ly <thonly@heartbank.ceo>
2022-09-07 17:09:10 -0700

    begin data structures

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import fs from 'fs';
 import https from 'https';
 import express from 'express';
 import cors from 'cors';
-import { getMovies, getShows } from "/library/index.mjs";
+import { getMovies, getShows } from "./library/index.mjs";
 
 // expires on 2022-10-13
 const cert = fs.readFileSync('private/fullchain.pem');
@@ -11,8 +11,8 @@ const key = fs.readFileSync('private/privkey.pem');
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.json({ movies: getMovies(), shows: getShows()});
+app.get('/', async (req, res) => {
+    res.json({ movies: await getMovies(), shows: await getShows()});
 });
 
 https.createServer({cert, key}, app).listen(444);
